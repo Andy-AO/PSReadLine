4933646128e0599130180b30132e05696f841c92
zen96285 <zen96285@gmail.com>
2022-05-10 18:47:48 +0800

    add serilog

--- PSReadLine/Singletons.cs
+++ PSReadLine/Singletons.cs
@@ -1,3 +1,8 @@
+using Serilog;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+
 namespace Microsoft.PowerShell.PSReadLine;
 
 public static class Singletons
@@ -5,6 +10,15 @@ public static class Singletons
     private static Renderer __renderer;
     public static History _hs => History.Singleton;
     public static PSConsoleReadLine _rl => PSConsoleReadLine.Singleton;
+    public static Logger logger { get; }
+
+    static Singletons()
+    {
+        logger = new LoggerConfiguration().MinimumLevel.Verbose()
+            .WriteTo.Console(outputTemplate:
+                @"{Timestamp:HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}")
+            .CreateLogger();
+    }
 
     public static Renderer _renderer
     {
