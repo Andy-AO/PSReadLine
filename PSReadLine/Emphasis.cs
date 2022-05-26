d316f07b1d5fa867b1ad3c60e6d179e1dca01801
zen96285 <zen96285@gmail.com>
2022-05-26 17:51:23 +0800

    add List<EmphasisRange> and Ranges

--- PSReadLine/Emphasis.cs
+++ PSReadLine/Emphasis.cs
@@ -1,13 +1,57 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Microsoft.PowerShell.PSReadLine;
 
+public record EmphasisRange
+{
+    private static readonly int MinimumValue = -1;
+    public readonly int End;
+    public readonly int Start;
+
+    public EmphasisRange(int start, int end)
+    {
+        IsValid(start, end);
+        End = end;
+        Start = start;
+    }
+
+    public void IsValid()
+    {
+        IsValid(Start, End);
+    }
+
+    private void IsValid(int start, int end)
+    {
+        var state = $"\nstart is {start}, end is {end}.";
+        if (start > end) throw new ArgumentException("The start must be less than the end." + state);
+
+        if (start < MinimumValue || end < MinimumValue)
+            throw new ArgumentException($"Index must be greater than or equal to {MinimumValue}" + state);
+    }
+
+    public bool IsIn(int index)
+    {
+        return Start <= index && index <= End;
+    }
+}
+
 public static class Emphasis
 {
+    private static List<EmphasisRange> _ranges = new();
+
     public static bool ToEmphasize(int index)
     {
         return index >= EmphasisStart &&
                index < EmphasisStart + EmphasisLength;
+    }
+
+    private static List<EmphasisRange> Ranges
+    {
+        get => _ranges.ToList();
+        set => _ranges = value.ToList();
     }
 
     internal static void EmphasisInit()
@@ -18,7 +62,6 @@ public static class Emphasis
 
     public static void SetEmphasisData(int startIndex, int length, CursorPosition p)
     {
-
         _renderer.Current = p switch
         {
             CursorPosition.Start => startIndex,
